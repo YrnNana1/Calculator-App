1256168a1e38e679f5af2e847bcaaa72b1185256
YrnNana1 <slyprempeh3@gmail.com>
2023-12-28 18:18:31 -0500

    edits to the test class

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -6,16 +6,19 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
+    @Test
     public void testAdd() {
         Calculator calculator = new Calculator();
         assertEquals(5.0, calculator.add(2.0, 3.0), 0.0001);
     }
 
+    @Test
     public void testSubtract() {
         Calculator calculator = new Calculator();
         assertEquals(3.0, calculator.subtract(5.0, 2.0), 0.0001);
     }
 
+    @Test
     public void testMultiply() {
         Calculator calculator = new Calculator();
         assertEquals(15.0, calculator.multiply(5.0, 3.0), 0.0001);
