ee585de3aa563aa3b9a394b28b7d47b56f0bcdf5
ethanoy <opanining13445@gmail.com>
2023-12-28 14:00:30 -0700

    methods for calculation

--- Calculator.java
+++ Calculator.java
@@ -3,14 +3,22 @@ public class Calculator {
         public double add(double num1, double num2){
             return num1 + num2;
         }
-    
         public double subtract(double num1, double num2){
             return num1 - num2;
         }
         public double multiply(double num1, double num2){
             return num1 * num2;
         }
-    
+        public double divide(double num1, double num2){
+            return num1 / num2;
+        }
+        public double percent(double num){
+            return num / 100;
+        }
+        public double negate(double num){
+            return num * -1;
+        }
+
     }
     
 
