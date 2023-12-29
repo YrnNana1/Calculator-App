5f2e2681c0bf3c3d1d33d16c2c9cc2ab6f4cac8c
ethanoy <opanining13445@gmail.com>
2023-12-29 16:30:09 -0700

    spacing and divide fix

--- Calculator.java
+++ Calculator.java
@@ -1,29 +1,23 @@
 public class Calculator {
+    
         private double result;
+
         public double add(double num1, double num2){
             return num1 + num2;
         }
+
         public double subtract(double num1, double num2){
             return num1 - num2;
         }
+
         public double multiply(double num1, double num2){
             return num1 * num2;
         }
-        public double divide(double num1, double num2){
-            try {
-                if (num2 == 0) {
-                    throw new ArithmeticException("Cannot divide by zero.");
-                }
-        
-                result = num1 / num2;
-            } catch (ArithmeticException e) {
-            }
-        
-            return result;
-        }
+
         public double percent(double num){
             return num / 100;
         }
+
         public double negate(double num){
             return num * -1;
         }
--- CalculatorTest.java
+++ CalculatorTest.java
@@ -2,7 +2,6 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 
-
 public class CalculatorTest {
 
     @Test
@@ -10,6 +9,7 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
         assertEquals(5.0, calculator.add(2.0, 3.0), 0.0001);
     }
+
     @Test
     public void testNegAdd() {
         Calculator calculator = new Calculator();
@@ -33,6 +33,7 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
         assertEquals(15.0, calculator.multiply(5.0, 3.0), 0.0001);
     }
+
     @Test
     public void testNegMultiply() {
         Calculator calculator = new Calculator();
@@ -40,37 +41,21 @@ public class CalculatorTest {
     }
 
     @Test
-    public void testDivide(){
-        Calculator calculator = new Calculator();
-        assertEquals(2.0, calculator.divide(6.0, 3.0), 0.0001);
-    }
-
-    @Test
-    public void testNegDivide(){
-        Calculator calculator = new Calculator();
-        assertEquals(-2.0, calculator.divide(-6.0, 3.0), 0.0001);
-    }
-
-    @Test
-    public void testDivideZero(){
-        Calculator calculator = new Calculator();
-        assertEquals(0, calculator.divide(10.0, 0.0), 0.0001);
-    }
-    @Test
     public void testPercent(){
         Calculator calculator = new Calculator();
         assertEquals(0.05, calculator.percent(5.0), 0.0001);
     }
+
     @Test 
     public void testNegate(){
         Calculator calculator  = new Calculator();
         assertEquals(-5.0, calculator.negate(5.0), 0.0001);
     }
+
     @Test 
     public void testNegate2(){
         Calculator calculator  = new Calculator();
         assertEquals(5.0, calculator.negate(-5.0), 0.0001);
     }
     
-
 } 
